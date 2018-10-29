5c1460f08a3cc8c190e811146c5ee8d5ca10d31c
flackbash <flack2bash@gmail.com>
2018-10-29 23:16:19 +0100

    Implement autoplay

---
+++ app/src/main/java/com/prangesoftwaresolutions/audioanchor/SettingsActivity.java
@@ -0,0 +1,31 @@
+package com.prangesoftwaresolutions.audioanchor;
+
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public static class AnchorPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            String stringValue = o.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+    }
+}
