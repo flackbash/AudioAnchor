ec09d1e9abc12d6210128e2eb42929be521e0929
Patryk Mis <patryk.mis@mykolab.com>
2022-04-16 01:44:42 +0200

    Migrate build system to Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,20 +7,13 @@ buildscript {
     }
 
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.1.2'
+        classpath("com.android.tools.build:gradle:7.1.2")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete("rootProject.buildDir")
 }
