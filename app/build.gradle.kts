cea6fe5d073859f4431bb8e048109934d361d2dc
Patryk Mis <patryk.mis@mykolab.com>
2022-08-04 18:03:02 +0200

    Bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.prangesoftwaresolutions.audioanchor"
         minSdk = 19
@@ -40,17 +40,17 @@ android {
 
 dependencies {
 // AndroidX
-    implementation("androidx.annotation:annotation:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-    implementation("androidx.core:core:1.7.0")
+    implementation("androidx.annotation:annotation:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core:1.8.0")
     implementation("androidx.media:media:1.6.0")
     implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.6.1")
 
 // Other
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.1")
     implementation("com.nambimobile.widgets:expandable-fab:1.2.1")
 }
