68ea6f0bb5be9c6b059727466b935e51df836840
Johannes Witzig <J0J0p@gmx.de>
2026-03-05 22:43:51 +0100

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.prangesoftwaresolutions.audioanchor"
         minSdk = 21
         targetSdk = 35
-        versionCode = 31
-        versionName = "2.4.0"
+        versionCode = 32
+        versionName = "2.5.0"
     }
     buildTypes {
         release {
